0e69e8d7d8e8c20ad06c50b141d540af0a7fdca5
rahulprops <rahulhack912@gmail.com>
2025-04-26 00:33:25 +0530

    DATABASE CONNECT SUCESSFUL

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config.js'
+import dbConnect from './confilg/db.js';
 const app = express()
 const port= process.env.PORT || 3000;
 
@@ -8,4 +9,5 @@ const port= process.env.PORT || 3000;
 //! server start
 app.listen(port,()=>{
     console.log(`server start on port ${port}`)
+    dbConnect()
 })
